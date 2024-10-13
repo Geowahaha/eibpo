e6f0bb3347787b5acce4c5f56634deed6b993199
Tawat Talpolkrang <35721377+Geowahaha@users.noreply.github.com>
2024-10-13 23:16:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function switchVideo(videoId) {
+    const videoSrcMap = {
+        'video1': 'https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto:best/powerwall-store-use-save-carousel-01.mp4',
+        'video2': 'https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto:best/powerwall-store-use-save-carousel-02.mp4',
+        'video3': 'https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto:best/powerwall-store-use-save-carousel-03.mp4'
+    };
+
+    const mainVideo = document.getElementById('main-video');
+    mainVideo.src = videoSrcMap[videoId];
+    mainVideo.play();
+}
+
 document.addEventListener('scroll', function() {
     const videoSections = document.querySelectorAll('.video-feature video');
     videoSections.forEach(video => {
